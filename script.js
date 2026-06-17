83bb470510d0af3ab977d3b7858484ed6d9f989a
rocioykevin <rociorome.18@gmail.com>
2026-06-17 16:22:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ function actualizarContador() {
 
 setInterval(actualizarContador, 1000);
 actualizarContador();
+function createStars() {
+  const numStars = 80;
+
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+
+    star.style.top = Math.random() * 100 + "vh";
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.animationDuration = (1 + Math.random() * 2) + "s";
+
+    document.body.appendChild(star);
+  }
+}
+
+createStars();

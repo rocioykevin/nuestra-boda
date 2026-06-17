90ca1ffedccaf48d5ab4449bacbc827a01af8d10
rocioykevin <rociorome.18@gmail.com>
2026-06-17 16:03:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const sobre = document.getElementById("sobre");
+const invitacion = document.getElementById("invitacion");
 
+sobre.addEventListener("click", () => {
+  invitacion.style.display = "block";
+  sobre.style.display = "none";
+  document.querySelector(".lechuza").style.display = "none";
+});
+
+function actualizarContador() {
+
+  const boda = new Date("April 3, 2027 00:00:00").getTime();
+  const ahora = new Date().getTime();
+
+  const diferencia = boda - ahora;
+
+  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+  document.getElementById("contador").innerHTML =
+    `⏳ ${dias} días · ${horas} horas · ${minutos} minutos · ${segundos} segundos`;
+}
+
+setInterval(actualizarContador, 1000);
+actualizarContador();
